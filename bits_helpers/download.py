dd184fd841ef4effc87f8141aa9cf8909c8e5813
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-08-13 12:14:30 +0200

    fix download with pip

--- bits_helpers/download.py
+++ bits_helpers/download.py
@@ -323,7 +323,6 @@ def download(source, dest, work_dir):
     match = urlTypeRe.match(source)
     if not urlTypeRe.match(source):
         raise MalformedUrl(source)
-    print("Debug:", source)
     downloadHandler = downloadHandlers[match.group(1)]
     filename = source.rsplit("/", 1)[1]
     downloadDir = join(cacheDir, checksum[0:2], checksum)
